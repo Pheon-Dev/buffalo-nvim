6736229906cb70f80a1236b38e6d165bc955447c
Pheon-Dev <devpheon@gmail.com>
2023-08-31 09:36:13 +0300

    [+] show total buffers

---
+++ lua/buffalo/init.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+function M.buffers()
+  local bufs = vim.api.nvim_list_bufs()
+  bufs = vim.tbl_filter(function(buf)
+    local is_loaded = vim.api.nvim_buf_is_loaded(buf)
+    local is_listed = vim.fn.buflisted(buf) == 1
+
+    if not (is_loaded and is_listed) then
+      return false
+    end
+
+    return true
+  end, bufs)
+  local count = 0
+  for _, buf in pairs(bufs) do
+    count = count + 1
+  end
+  return count
+end
+
+return M
