bb46e028eec587d51c9ea8a2d0071d027ff08ecd
Pheon-Dev <devpheon@gmail.com>
2023-09-02 12:17:51 +0300

    [!] buffer count [-] debug print

--- lua/buffalo/init.lua
+++ lua/buffalo/init.lua
@@ -11,6 +11,16 @@ M.marks = {}
 
 function M.buffers()
   local bufs = vim.api.nvim_list_bufs()
+  bufs = vim.tbl_filter(function(buf)
+    local is_loaded = vim.api.nvim_buf_is_loaded(buf)
+    local is_listed = vim.fn.buflisted(buf) == 1
+
+    if not (is_loaded and is_listed) then
+      return false
+    end
+
+    return true
+  end, bufs)
   local count = 0
   for _, buf in pairs(bufs) do
     count = count + 1
@@ -21,7 +31,6 @@ end
 function M.tabpages()
   local tabs = vim.api.nvim_list_tabpages()
   local count = 0
-  print(tabs)
   for _, tab in pairs(tabs) do
     count = count + 1
   end
